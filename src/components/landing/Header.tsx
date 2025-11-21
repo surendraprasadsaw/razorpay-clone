0c81a03958cfe1e5e8994f595bbefe046e7fdd75
Surendra Prasad saw <prasadsaw1999@gmail.com>
2025-11-21 16:40:02 +0000

    add more  margin form the left

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -23,7 +23,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center pl-8">
+      <div className="container flex h-16 items-center pl-12">
         <div className="mr-4 flex items-center">
           <Link href="/" className="font-bold text-2xl">Razor Pay</Link>
         </div>
